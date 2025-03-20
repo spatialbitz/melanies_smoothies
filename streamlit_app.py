aa794380bc6056bbcced10db6e7c7a087c889c74
spatialbitz <spatialbitz@users.noreply.github.com>
2025-03-20 17:19:50 +0100

    Update streamlit_app.py
    session set up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 helpful_links = [
@@ -16,7 +15,9 @@ st.write(
     f"""Orders that need to be filled.
     """)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+#session = get_active_session()
 #my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 
